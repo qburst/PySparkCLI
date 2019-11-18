bdb0b3a59df9a576c4747352eaffeff4bb7b9053
Jino Jossy <jinoj@qburst.com>
2019-11-18 17:09:04 +0530

    started building the cli to initiate start project command

--- pysparkcli/bin/start.py
+++ pysparkcli/bin/start.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python
+import sys
+import click
+import os
+
+from pathlib import Path
+
+@click.group()
 def start():
-	print("Hello world!")
+	BASE_PATH = Path(__file__)
+	PROJECT_TEMPLATE_PATH = BASE_PATH.resolve(strict=True).parents[0] / "project-template" / "project_name"
+	print(PROJECT_TEMPLATE_PATH)
+	print(BASE_PATH)
+
+	print(sys.argv)
+	print("?"*40)
+	print(PROJECT_TEMPLATE_PATH.resolve())
+
+@start.command(help="Create Project")
+@click.option("--master", help="Enter master URL", required=True)
+@click.option("--name", help="Enter Project Name", required=True)
+@click.option("--cores", help="Enter number of core", type=click.INT)
+def create(master, name, cores):
+	click.echo("Master: {}".format(master))
+	click.echo("\nName: {}".format(name))
+	click.echo("\nCores: {}".format(cores))
+
+	proj_path =	Path(name)
+	proj_path.mkdir()
+
 
 if __name__ == "__main__":
 	start()
--- pysparkcli/core/admin/__init__.py
+++ pysparkcli/core/admin/__init__.py
@@ -1,3 +1,19 @@
+import os
+import sys
+
+
+class CommandUtility:
+    """
+    Logic for building commands pysparkcli utilities.
+    """
+    def __init__(self, argv=None):
+        self.argv = argv or sys.argv[:]
+        self.prog_name = os.path.basename(self.argv[0])
+        if self.prog_name == '__main__.py':
+            self.prog_name = 'python -m django'
+        self.settings_exception = None
+
+
 def execute_from_command_line(argv=None):
     """Run a ManagementUtility."""
     print("{*}*40")
