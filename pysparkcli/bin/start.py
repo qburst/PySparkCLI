bad4fe81d646fb52216a499fe7c85d012dd0d374
Jino Jossy <jinoj@qburst.com>
2019-11-22 17:21:12 +0530

    remove print statements

--- pysparkcli/bin/start.py
+++ pysparkcli/bin/start.py
@@ -14,18 +14,9 @@ def start():
 @click.option("--name", help="Enter Project Name", required=True)
 @click.option("--cores", help="Enter number of core", type=click.INT)
 def create(master, name, cores):
-	click.echo("Master: {}".format(master))
-	click.echo("\nName: {}".format(name))
-	click.echo("\nCores: {}".format(cores))
 
 	BASE_PATH = Path(__file__)
 	PROJECT_TEMPLATE_PATH = BASE_PATH.resolve(strict=True).parents[1] / "project-template" / "project_name"
-	print(PROJECT_TEMPLATE_PATH)
-	print(BASE_PATH)
-
-	print(sys.argv)
-	print("?" * 40)
-	print(PROJECT_TEMPLATE_PATH.resolve())
 
 	context = {
 		"project_name": name,
@@ -39,4 +30,5 @@ def create(master, name, cores):
 
 
 if __name__ == "__main__":
+	# Start the execution of command here
 	start()
