1436d1e17ae27820704ed288ad06eb14bdc0ad93
Jino Jossy <jinojossy93@gmail.com>
2019-12-03 13:56:16 +0530

    Update PIP setup file and correct the tamplate path

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     packages=find_namespace_packages(include=['core.*', 'bin.*']),
     version='0.0.1',
     description='PySpark Project Buiding Tool',
-    url='http://code.qburst.com/jinoj/pysparkcli',
-    author='Jino J',
-    author_email='jinoj@qburst.com',
+    url='https://github.com/qburst/PySparkCLI',
+    author='Jino Jossy',
+    author_email='jinojossy93@gmail.com',
     license='MIT',
     py_modules=['pysparkcli', 'core', 'bin'],
     install_requires=[
