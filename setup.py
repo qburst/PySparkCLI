a172780aa048243b8c7594ff97e314d5f95bc7f6
Jino Jossy <jinojossy93@gmail.com>
2019-12-05 00:29:29 +0530

    Added MANIFEST.in and update PIP setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import os
+import setuptools
 
-
-from setuptools import setup, find_namespace_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-setup(
+setuptools.setup(
     name="pysparkcli",
-    packages=find_namespace_packages(include=['core.*', 'bin.*']),
     version='0.0.1',
     description='PySpark Project Buiding Tool',
     url='https://github.com/qburst/PySparkCLI',
@@ -17,11 +15,13 @@ setup(
     long_description=README,
     long_description_content_type="text/markdown",
     license='MIT',
-    py_modules=['pysparkcli', 'core', 'bin'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'pyspark',
         'click',
         'jinja2',
+        'pathlib==1.0.1'
     ],
     entry_points='''
         [console_scripts]
