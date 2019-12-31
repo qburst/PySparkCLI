46331b14df1100544787c83272ab7cb535f1590f
chandan-urs <chandan@qburst.com>
2019-12-31 14:26:30 +0530

    Added database config

--- examples/twitter-streaming/SparkDemo.py
+++ examples/twitter-streaming/SparkDemo.py
@@ -10,22 +10,10 @@ import time
 import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
-import db
-import psycopg2
-
-
-def save_tweet(json_tweet):
-    try:
-        conn = db.connect()
-        sql = """insert into tweet(username) values(%s);"""
-        cur = conn.cursor()
-        print(json_tweet["user"]["name"], json_tweet["id"])
-        cur.execute(sql, (json_tweet["user"]["name"],))
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        db.close(conn)
+import database
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrameWriter
 
 
 # Our filter function:
@@ -39,8 +27,17 @@ def filter_tweets(tweet):
     if json_tweet.get('lang'):  # When the lang key was not present it caused issues
         if json_tweet['lang'] == 'en':
             print("english tweet")
-            # print(json_tweet)
-            save_tweet(json_tweet);
+            print(json_tweet)
+            print("end tweet")
+            spark = SparkSession \
+                .builder \
+                .appName("Python Spark SQL data source example") \
+                .getOrCreate()
+            df = pd.DataFrame(json_tweet)
+            ddf = spark.createDataFrame(df)
+            spark.write.jdbc("jdbc:postgresql:"+db.url+"/"+db.database, "schema.twitterdata",
+                          properties={"user": db.user, "password": db.password})
+
             return True  # filter() requires a Boolean value
     return False
 
@@ -51,6 +48,8 @@ ssc = StreamingContext(sc, 10)  # 10 is the batch interval in seconds
 IP = "localhost"
 Port = 5555
 lines = ssc.socketTextStream(IP, Port)
+db = database.config()
+
 
 # When your DStream in Spark receives data, it creates an RDD every batch interval.
 # We use coalesce(1) to be sure that the final filtered RDD has only one partition,
@@ -61,8 +60,6 @@ lines = ssc.socketTextStream(IP, Port)
 # it will throw an Exception.
 
 lines.foreachRDD(lambda rdd: rdd.filter(filter_tweets).coalesce(1).saveAsTextFile("./tweets/%f" % time.time()))
-# lines.foreachRDD(lambda rdd: rdd.filter(filter_tweets).coalesce(1))
 
-# You must start the Spark StreamingContext, and await process termination…
 ssc.start()
 ssc.awaitTermination()
