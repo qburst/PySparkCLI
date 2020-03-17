158b8dedface40d948a703a157ab1acf30faa4a0
chandan-urs <p.chandan19952@gmail.com>
2020-03-17 12:04:58 +0530

    Linear Regression fortune 500 added.

---
+++ examples/linear_regression_fortune500/README.md
@@ -0,0 +1,17 @@
+## Linear Regression Fortune 500 - PySpark ML Project
+
+#### Installation:
+```python
+pip install pysparkcli
+```
+
+#### Run the project:
+```python
+pysparkcli run linear_regression_fortune500
+```
+
+#### Note:
+The input data for the project must be placed in src/InputData folder.
+
+### References:
+https://intellipaat.com/blog/tutorial/spark-tutorial/machine-learning-with-pyspark-tutorial/
---
+++ examples/linear_regression_fortune500/src/app.py
@@ -0,0 +1,55 @@
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import regexp_replace
+from pyspark.ml.regression import LinearRegression
+
+
+## Supporting Functions
+
+
+## Core Spark Application Functionality
+
+def main(sqlContext):
+
+
+
+    company_df = sqlContext.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(
+        './linear_regression_fortune500/src/InputData/Fortune_500.csv')
+    company_df = company_df.withColumn('Number of Employees', regexp_replace('Number of Employees', ',', ''))
+    company_df = company_df.withColumn('Number of Employees', company_df['Number of Employees'].cast("int"))
+    company_df.show(10)
+
+    company_df.cache()
+    company_df.printSchema()
+
+    company_df.describe().toPandas().transpose()
+
+
+    company_df.cache()
+    company_df.printSchema()
+
+    company_df.show(10)
+
+    vectorAssembler = VectorAssembler(inputCols=['Rank', 'Number of Employees'], outputCol='features')
+    tcompany_df = vectorAssembler.setHandleInvalid("keep").transform(company_df)
+    tcompany_df = tcompany_df.select(['features', 'Number of Employees'])
+    tcompany_df.show(10)
+
+    splits = tcompany_df.randomSplit([0.7, 0.3])
+    train_df = splits[0]
+    test_df = splits[1]
+
+    lr = LinearRegression(featuresCol='features', labelCol='Number of Employees', maxIter=10, regParam=0.3,
+                          elasticNetParam=0.8)
+    lr_model = lr.fit(train_df)
+    print("Coefficients: " + str(lr_model.coefficients))
+    print("Intercept: " + str(lr_model.intercept))
+
+
+if __name__ == "__main__":
+    # Configure Spark Application
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    # Execute Main functionality
+    main(sqlContext)
