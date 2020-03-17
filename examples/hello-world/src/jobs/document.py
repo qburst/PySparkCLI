b01e2455652a1d73487d79f305604c325bd0accb
chandan-urs <p.chandan19952@gmail.com>
2020-03-17 11:09:55 +0530

    Fixed conflicts

--- examples/hello-world/src/jobs/document.py
+++
@@ -1,8 +0,0 @@
-from mongoengine import *
-connect('mydb2')
-
-
-class EtlData(Document):
-    name = StringField(required=True, max_length=200)
-    steps_to_desk = IntField()
-    meta = {'allow_inheritance': True}
--- examples/hello-world/src/jobs/etl_job.py
+++ examples/hello-world/src/jobs/etl_job.py
@@ -40,9 +40,6 @@ from pyspark.sql import Row
 from pyspark.sql.functions import col, concat_ws, lit
 
 from pysparkcli.core.admin import SparkBuilder
-from document import EtlData as MongoEtlData
-from sql_db import EtlData as SQLEtlData
-from sql_db import getSession
 
 
 def main():
@@ -62,9 +59,6 @@ def main():
     # execute ETL pipeline
     data = extract_data(spark)
     data_transformed = transform_data(data, config.get('steps_per_floor', 12))
-    data = data_transformed.collect()
-    load_data_to_mongo(data)
-    load_data_to_db(data)
     load_data(data_transformed)
 
     # log the success and terminate Spark application
@@ -159,20 +153,6 @@ def create_test_data(spark, config):
      .parquet('tests/test_data/employees_report', mode='overwrite'))
 
     return None
-
-
-def load_data_to_mongo(data):
-    for row in data:
-        etl = MongoEtlData(name=row["name"], steps_to_desk=row["steps_to_desk"])
-        etl.save()
-
-
-def load_data_to_db(data):
-    session = getSession()
-    for row in data:
-        etl = SQLEtlData(name=row["name"], steps_to_desk=row["steps_to_desk"])
-        session.add(etl)
-    session.commit()
 
 
 # entry point for PySpark ETL application
